46824e70c5208777a2ca4e090070c0e323806bb5
Steven Best <steven.best1@yahoo.com>
2020-12-03 10:42:54 +0000

    Pre-Proc: Two stage character segmentation
    anpr_pre_processing:
      - Detect potential license plate contours
        - First approach from OCR implementation
        - Second from acedemic paper: http://www.mecs-press.org/ijisa/ijisa-v8-n3/v8n3-2.html
      - Segment each plate character from contours
        - Select contour with 7 characters
        - Write character images to be manually sorted or (tested on SVM)
      - Problems
        - Only works on plates with min 7 characters
        - Needs tuning for some cases
        - Some contours return images that are not characters
    
    anpr_svm:
      - Modified to match the new folder structure of train and test images
      - Prints training and test accuracy
      - Problems:
        - Need more training data
        - Specifically characters with < 10 images
        - e.g. U predicted as 0 and N predictes as W

---
+++ anpr_pre_processing.py
@@ -0,0 +1,243 @@
+# Import necessary packages
+import cv2
+import os
+import re
+import imutils
+import argparse
+import numpy as np
+from matplotlib import pyplot as plt
+from natsort import natsorted, ns
+from skimage.segmentation import clear_border
+
+"""
+LocateAndDivideNumberPlate: Class that locates and sub divides the
+                            characters from the vehicle number plate and
+                            converts them into sub-images for SVM
+"""
+class LocateAndDivideNumberPlate:
+    def __init__(self, path_to_images = './images/', debug = False):
+        self.path_to_images = path_to_images
+        self.global_char_count = 0
+        self.char_count = 0
+        self.debug = debug
+
+    def show_image(self, title, image, waitKey=False):
+        if self.debug:
+            cv2.imshow(title, image)
+            if waitKey:
+                cv2.waitKey(0)
+
+    def find_and_divide(self, image_path):
+        image = cv2.imread(self.path_to_images + image_path)
+        image = imutils.resize(image, width=600)
+        image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        contour_list_v1 = self.locate_number_plate(image_gray, 12)
+        contour_list_v2 = self.locate_number_plate_v2(image_gray, 12)
+
+        characters_segmented = self.locate_number_plate_characters(contour_list_v1 + contour_list_v2)
+
+        if not characters_segmented:
+            # Try clearing border pixels on contour
+            characters_segmented = self.locate_number_plate_characters(contour_list_v1 + contour_list_v2, True)
+
+        if not characters_segmented:
+            # Failure!
+            print("Unable to locate or segment number plate characters!")
+
+    def locate_number_plate_v2(self, image, num_contours=5):
+        cv2.destroyAllWindows()
+        bil = cv2.bilateralFilter(image, 15, 50, 75);
+        self.show_image("Bilateral Filter", bil)
+
+        clahe_filter = cv2.createCLAHE(clipLimit=1.0, tileGridSize=(8,8))
+        clahe = clahe_filter.apply(bil)
+        self.show_image("Adaptive HE", clahe)
+
+        disc_kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (9,9))
+        square_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,4))
+        opening = cv2.morphologyEx(clahe, cv2.MORPH_OPEN, disc_kernel)
+        self.show_image("Circular Opening", opening)
+
+        ahe_minus_open = clahe - opening
+        self.show_image("AHE - Opening", ahe_minus_open)
+
+        binarise = cv2.threshold(ahe_minus_open, 0, 255,
+            cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+        self.show_image("Binarise", binarise)
+
+        sobel_dy = cv2.Sobel(binarise, ddepth=cv2.CV_32F,
+            dx=0, dy=1, ksize=-1)
+        sobel_dx = cv2.Sobel(binarise, ddepth=cv2.CV_32F,
+            dx=0, dy=1, ksize=-1)
+        abs_sobel_x = cv2.convertScaleAbs(sobel_dx)
+        abs_sobel_y = cv2.convertScaleAbs(sobel_dy)
+        sobel = cv2.addWeighted(abs_sobel_x, 0.5, abs_sobel_y, 0.5, 0)
+        self.show_image("Sobel", sobel)
+
+        dilate = cv2.dilate(sobel, None, iterations=1)
+        self.show_image("Dilate", dilate)
+
+        dilate_copy = dilate.copy()
+        h, w = dilate_copy.shape[:2]
+        flood_mask = np.zeros((h+2, w+2), np.uint8)
+        cv2.floodFill(dilate_copy, flood_mask, (0,0), 255);
+        floodfill_bitwise = cv2.bitwise_not(dilate_copy)
+        floodfill_im = dilate | floodfill_bitwise
+        self.show_image("Fill plate", floodfill_im)
+
+        erode = cv2.erode(floodfill_im, None, iterations=1)
+        self.show_image("Erode", erode, True)
+
+        # Find the contours in the image
+        contours = cv2.findContours(erode.copy(), cv2.RETR_EXTERNAL,
+                                    cv2.CHAIN_APPROX_SIMPLE)
+        contours = imutils.grab_contours(contours)
+        contours = sorted(contours, key=cv2.contourArea, reverse=True)[:num_contours]
+        contour_list = []
+        for c in contours:
+            (x, y, w, h) = cv2.boundingRect(c)
+            ratio = float(w/h)
+            if ratio >= 2.9 and ratio <= 6.2:
+                contour_image = image[y:y + h, x:x + w]
+                contour_list.append(contour_image)
+                self.show_image("Plate Contour", contour_image)
+                cv2.waitKey(0)
+
+        return contour_list
+
+    def locate_number_plate(self, image, num_contours=5):
+        cv2.destroyAllWindows()
+        # Blackhat morphological operation
+        #   - reveal dark regions (i.e., text) on light backgrounds
+        rectKern = cv2.getStructuringElement(cv2.MORPH_RECT, (30 , 5))
+        blackhat = cv2.morphologyEx(image, cv2.MORPH_BLACKHAT, rectKern)
+        self.show_image("Blackhat", blackhat)
+
+        # Closing then thresholding
+        #   - find light regions
+        squareKern = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+        light = cv2.morphologyEx(image, cv2.MORPH_CLOSE, squareKern)
+        light = cv2.threshold(light, 0, 255,
+            cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+        #light = cv2.erode(light, None, iterations=1)
+        self.show_image("Light Regions", light)
+
+        # compute the Scharr gradient of blackhat
+        #   - in x-direction
+        #   - scale back to [0,255]
+        gradX = cv2.Sobel(blackhat, ddepth=cv2.CV_32F,
+            dx=1, dy=0, ksize=-1)
+        gradX = np.absolute(gradX)
+        (minVal, maxVal) = (np.min(gradX), np.max(gradX))
+        gradX = 255 * ((gradX - minVal) / (maxVal - minVal))
+        gradX = gradX.astype("uint8")
+        self.show_image("Scharr", gradX)
+
+        # 1. blur the gradient representation
+        # 2. applying a closing
+        # 2. binarise
+        gradX = cv2.GaussianBlur(gradX, (5, 5), 0)
+        gradX = cv2.morphologyEx(gradX, cv2.MORPH_CLOSE, rectKern)
+        thresh = cv2.threshold(gradX, 0, 255,
+            cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+        self.show_image("Grad Thresh", thresh)
+
+        # Erode and dilate
+        #   - remove noise
+        thresh = cv2.erode(thresh, None, iterations=2)
+        thresh = cv2.dilate(thresh, None, iterations=2)
+        self.show_image("Grad Erode/Dilate", thresh)
+
+        # take the bitwise AND between the threshold result and the
+        # light regions of the image
+        thresh = cv2.bitwise_and(thresh, thresh, mask=light)
+        thresh = cv2.dilate(thresh, None, iterations=2)
+        thresh = cv2.erode(thresh, None, iterations=1)
+        self.show_image("Final", thresh, True)
+
+        # Find the contours in the image
+        contours = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+                                    cv2.CHAIN_APPROX_SIMPLE)
+        contours = imutils.grab_contours(contours)
+        contours = sorted(contours, key=cv2.contourArea, reverse=True)[:num_contours]
+        contour_list = []
+        for c in contours:
+            (x, y, w, h) = cv2.boundingRect(c)
+            ratio = float(w/h)
+            if ratio >= 2.9 and ratio <= 6.2:
+                contour_image = image[y:y + h, x:x + w]
+                contour_list.append(contour_image)
+                self.show_image("Plate Contour", contour_image)
+                cv2.waitKey(0)
+        return contour_list
+
+    def locate_number_plate_characters(self, contour_list, clear_border_pixels=False, num_contours=15):
+        self.char_count = 0
+        print("Locating characters...")
+        cv2.destroyAllWindows()
+        for image in contour_list:
+            # Reveal dark regions on light regions (text on plate)
+            rectKern = cv2.getStructuringElement(cv2.MORPH_RECT, (30, 3))
+            blackhat = cv2.morphologyEx(image, cv2.MORPH_BLACKHAT, rectKern)
+            self.show_image("Blackhat", blackhat)
+
+            # Binarise image
+            binarised = cv2.adaptiveThreshold(blackhat, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 15, 10)
+            self.show_image("Binarised Image", binarised)
+
+            if clear_border_pixels:
+                cb = clear_border(binarised)
+                self.show_image("Clear border pixels", cb, True)
+
+            # Find the contours in the image
+            if clear_border_pixels:
+                contours = cv2.findContours(cb.copy(), cv2.RETR_EXTERNAL,
+                                            cv2.CHAIN_APPROX_SIMPLE)
+            else:
+                contours = cv2.findContours(binarised.copy(), cv2.RETR_EXTERNAL,
+                                            cv2.CHAIN_APPROX_SIMPLE)
+            contours = imutils.grab_contours(contours)
+            contours = sorted(contours, key=cv2.contourArea, reverse=True)[:num_contours]
+
+            char_images = []
+            for c in contours:
+                (x, y, w, h) = cv2.boundingRect(c)
+                ratio = float(h/w)
+                contour_image = image[y:y + h, x:x + w]
+                if (ratio >= 1.3 and ratio <= 2.3) or (ratio >= 3.7 and ratio <= 6.0):
+                    self.char_count += 1 # Character found
+                    self.global_char_count += 1 # Character found
+                    char_images.append(contour_image)
+                    print(self.char_count)
+                    self.show_image("Plate Character", contour_image, True)
+                    # hist = cv2.calcHist([contour_image],[0],None,[256],[0,256])
+                    # plt.plot(hist)
+                    # plt.xlim([0,256])
+                    # plt.show()
+
+            if self.char_count < 7:
+                print(str(self.char_count) + " characters found")
+                self.global_char_count -= self.char_count
+                self.char_count = 0
+            else:
+                print("Found all characters!")
+                for idx, image in enumerate(char_images):
+                    cv2.imwrite(self.path_to_images + 'located_characters/char_' + str(self.global_char_count - self.char_count + idx) + '.jpg', image)
+                return True
+
+        return False # Unable to segment 7 characters from contour list
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-d", required=True)
+arg = vars(ap.parse_args())
+
+pre_proc = LocateAndDivideNumberPlate(debug = int(arg["d"]))
+# Iterate through images and generate dataset
+car_images = natsorted(os.listdir('./images/uk'))
+remove_folders = re.compile('^.*\.(jpg)$')
+car_images = [i for i in car_images if remove_folders.match(i)]
+print(car_images)
+for image in car_images:
+    print("\nLocating and dividing number plate in: " + image)
+    pre_proc.find_and_divide('./uk/' + image)
--- anpr_svm.py
+++ anpr_svm.py
@@ -1,5 +1,6 @@
 # Import ML, image and graphing libraries
 import os
+import re
 import numpy as np
 import argparse
 import imutils
@@ -8,6 +9,8 @@ from natsort import natsorted, ns
 from skimage import io
 from sklearn import svm, metrics
 import matplotlib.pyplot as plt
+# Import pre-processing class
+# from anpr_pre_processing import LocateAndDivideNumberPlate
 
 # Import and process training and test data (and labels for training)
 def import_data(args):
@@ -15,20 +18,31 @@ def import_data(args):
     train_labels = []
     test_list = []
     test_labels = []
-    train_files = natsorted(os.listdir(args["input"] + '/train_characters/'))
-    test_files = natsorted(os.listdir(args["input"] + '/test_characters/'))
-    # Get training labels
-    for label in train_files:
-        train_labels.append(label[0]) # S or 5
+
+    train_dir = args["input"] + '/train_characters/'
+    test_dir = args["input"] + '/test_characters/'
+
+    train_files = natsorted(os.listdir(train_dir))
+    not_folder = re.compile('^.*\.(py)$')
+    train_files = [i for i in train_files if not not_folder.match(i)]
+    test_files = natsorted(os.listdir(test_dir))
+
+    for im_folder in train_files:
+        char_dir = train_dir + im_folder + '/'
+        char_images = natsorted(os.listdir(char_dir))
+        # Get training labels and images
+        for label in char_images:
+            train_labels.append(label[0])
+            train_list.append(cv2.resize(cv2.imread(char_dir + label, 0), (35,60)))
+
     # Get training labels
     for label in test_files:
         test_labels.append(label[0]) # S or 5
-    # Add character images to the train list
-    for c in train_files:
-        train_list.append(cv2.resize(cv2.imread(args["input"] + '/train_characters/' + c, 0), (35,60)))
+
     # Add character images to the test list
     for c in test_files:
-        test_list.append(cv2.resize(cv2.imread(args["input"] + '/test_characters/' + c, 0), (35,60)))
+        test_list.append(cv2.resize(cv2.imread(test_dir + c, 0), (35,60)))
+
     return (train_list, train_labels, test_list, test_labels)
 
 # Create Support Vector Machine classifier
@@ -45,20 +59,36 @@ def main():
 
     print("Importing training and test data...")
     (characters, labels, test, test_labels) = import_data(args)
-    flatten_chars = np.reshape(characters, (20, 35*60))
+    flatten_chars = np.reshape(characters, (len(characters), 35*60))
     flatten_test = np.reshape(test, (len(test), 35*60))
+
     # for c in characters:
     #     cv2.imshow('Character', c)
     #     cv2.waitKey(0)
+
     print("Building & training SVM classifier...")
     svc = build_SVM(flatten_chars, labels);
+
     print("\t\t---------Validating training data---------")
     pred = svc.predict(flatten_chars)
-    print("Predicted:\n" + str(list(pred)))
-    print("Actual:\n" + str(labels))
+    training_accuracy = 0
+    for idx, p in enumerate(pred):
+        if p == labels[idx]:
+            training_accuracy += 1
+    training_accuracy /= len(pred)
+    training_accuracy *= 100
+    print("Training Accuracy: " + str(training_accuracy) + "%")
+
     print("\n\t\t---------Predicting test data---------")
     pred = svc.predict(flatten_test)
     print("Predicted:\n" + str(list(pred)))
     print("Actual:\n" + str(test_labels))
+    test_accuracy = 0
+    for idx, p in enumerate(pred):
+        if p == test_labels[idx]:
+            test_accuracy += 1
+    test_accuracy /= len(pred)
+    test_accuracy *= 100
+    print("Testing Accuracy: " + str(test_accuracy) + "%")
 
 main() # Start ANPR
