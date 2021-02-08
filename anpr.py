f2a296f9db9504ba5b622673156b4bbcad5378d0
Steven Best <steven.best1@yahoo.com>
2021-02-08 22:52:20 +0000

    ANPR: Filter out edge 'noise' characters based on gap between characters
    Approximately 50% success rate
    Try removing border pixels in cases that don't work

--- anpr.py
+++ anpr.py
@@ -20,25 +20,75 @@ def getANPR(image_list, position_list, d):
     # Sort by x position in image
     for idx, pos in enumerate(sorted_position_list): sorted_position_list[idx] = [idx, pos]
     sorted_position_list.sort(key=lambda x: x[1])
-    # Add distance (pixels) between characters (excluding last)
-    for idx, pos in enumerate(sorted_position_list[:num_chars-1]):
-        pos.append(sorted_position_list[idx+1][1] - (pos[1] + position_list[pos[0]][2]) )
+    # Add distance (pixels) between characters
+    #   - [Left, Right]
+    for idx, pos in enumerate(sorted_position_list):
+        if idx == 0 :
+            pos.append(0.0) # Nothing to left
+            pos.append(sorted_position_list[idx+1][1] - (pos[1] + position_list[pos[0]][2]) )
+        elif idx == num_chars-1:
+            pos.append(pos[1] - (sorted_position_list[idx-1][1] + position_list[sorted_position_list[idx-1][0]][2]) )
+            pos.append(0.0) # Nothing to right
+        else:
+            pos.append(pos[1] - (sorted_position_list[idx-1][1] + position_list[sorted_position_list[idx-1][0]][2]) )
+            pos.append(sorted_position_list[idx+1][1] - (pos[1] + position_list[pos[0]][2]) )
+    print(sorted_position_list)
+
     # Determine mean and std
-    mean = 0
-    std = 0
+    #   - Right side gaps
+    mean_r = 0
+    std_r = 0
     for img in sorted_position_list[:num_chars-1]:
-        mean += img[2]
-    mean /= (num_chars-1)
+        mean_r += img[2]
+    mean_r /= (num_chars-1)
     for img in sorted_position_list[:num_chars-1]:
-        std += math.pow(img[2]-mean, 2)
-    std = math.sqrt(std/(num_chars-2))
+        std_r += math.pow(img[2]-mean_r, 2)
+    std_r = math.sqrt(std_r/(num_chars-2))
     # Ideal character distance range
-    upper_d = mean + std
-    lower_d = mean - std
-    if d == True:
-        print(sorted_position_list)
-        print('Mean(u): ' + str(mean),'Std: ' + str(std))
-        print('u+std: ' + str(upper_d),'u-std: ' + str(lower_d))
+    upper_d_r = mean_r + (std_r)
+    lower_d_r = mean_r - (std_r)
+    print("Gaps between characters:")
+    print('\tMean(u): ' + str(mean_r),'Std: ' + str(std_r))
+    print('\tu+0std: ' + str(upper_d_r),'u-std: ' + str(lower_d_r))
+
+    # Determine center gap of plate and noise characters
+    d_char = [] # Dirty bit characters
+    char_counter = 0
+    centre_index = False
+    for idx, char in enumerate(sorted_position_list):
+        if idx == 0:
+            if (char[3] >= lower_d_r) and (char[3] <= upper_d_r):
+                d_char.append(0)
+                char_counter += 1
+            else:
+                d_char.append(1)
+        elif idx == num_chars-1:
+            if (char[2] >= lower_d_r) and (char[2] <= upper_d_r):
+                d_char.append(0)
+                char_counter += 1
+            else:
+                d_char.append(1)
+                if d_char[idx-1] == 1:
+                    d_char[idx-1] = 0
+        else:
+            if ((char[3] >= lower_d_r) and (char[3] <= upper_d_r)) and ((char[2] >= lower_d_r) and (char[2] <= upper_d_r)):
+                d_char.append(0)
+                char_counter += 1
+            else:
+                if d_char[idx-1] == 1:
+                    d_char.append(0)
+                    char_counter += 1
+                elif char_counter == 3:
+                    d_char.append(0)
+                    char_counter = 0
+                    centre_index = char[0]
+                elif centre_index == sorted_position_list[idx-1][0]:
+                    d_char.append(0)
+                else:
+                    d_char.append(1)
+    print(d_char)
+
+
 
     print("Importing ANPR SVM Classifier...")
     svc = load("./trained_models/ANPR_SVM_v1.joblib")
